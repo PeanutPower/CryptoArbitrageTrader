67435e4504c336bdbb2206cc78289bfaf85b6a8a
PeanutPower <support@ltcgaming.tk>
2013-09-08 19:28:25 -0700

    update default config to exclude crypto-trade and cryptsy (not working) use singlequotes throughout for consistency

--- config.py
+++ config.py
@@ -17,7 +17,8 @@ from crypto import crypto
 FEE = 1.02 # fee for every trade (2%)
 Diff = 1.02 # 2 % arbitrage to execute
 curr = ["ltc","ftc","ppc","nvc","nmc"] #currencies to trade if arbitrage is found
-exc = ["btc-e", "vircu", "cryptsy", "coins-e"] #exchanges to trade on for the function calls
+exc = ["btc-e", "vircu", "coins-e"] #exchanges to trade on for the function calls
+# also "cryptsy" and "crypto" (but not working for me as of 9/9/2013)
 amount1 = 10 #the number of altcoins to be traded vs. btc in each trade
 
 # BTC-e login data:
@@ -26,16 +27,16 @@ secret = 'secret_key'
 btce = btc_e_api.API(key,secret)
 
 #Vircurex Login Data:
-vircurex = Account("username", "api_secret")
+vircurex = Account('username', 'api_secret')
 
 #Crypto-trade Login Data:
-key="Public_Key"
-secret="Secret_Key"
+key='Public_Key'
+secret='Secret_Key'
 cryp = crypto(key,secret)	
 
 #Cryptsy Login Data:
 cryptsy = PyCryptsy('Public_Key', 'Secret_Key')
 
 #coins-e Login Data:
-PUBLIC_KEY = "Public_Key"  
-PRIVATE_KEY = "Secret_Key"
+PUBLIC_KEY = 'Public_Key' 
+PRIVATE_KEY = 'Secret_Key'
